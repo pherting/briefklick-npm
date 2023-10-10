c160fa28d08c9f751a549acdcae873b199ff6a95
Patrick Herting <pher@live.de>
2023-10-10 20:08:10 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # briefklick-npm
 NodeJS NPM Package for BriefKlick API
+
+# how to use
+
+const briefklick = require('briefklick');
+
+const bk = new briefklick("YOURAPIKEYHERE");
+
+bk.getBalance().then((res) => {
+    console.log(res); // return Balance in €
+});
+
+
+// sender: string of your adress like: Max Musterman, Musterstr. 12, 13371 Musterstadt
+// receiver: string of valid german adress escaped with \n like: Max Musterman\nMusterstr. 12\n13371 Musterstadt
+// html: string of you html text
+// extrapage: 0 for no extrapage, 1 for extrapage with adress details on front of your html
+bk.createDocument(sender, receiver, html, extraPage).then((res) => {
+    console.log(res); // return documentId and page counts 
+});
+
+
+// documentId:  result of createDocument
+bk.previewDocument(documentId).then((res) => {
+    console.log(res); // return pdf file as base64 string and page counts 
+});
+
+
+// documentId: result of createDocument
+/* mode:
+    0 = Normal
+    1 = Einschreiben Einwurf
+    2 = Einschreiben Standard
+    3 = Einschreiben Eigenhändig
+*/
+// color: 0 for Black&White, 1 for colored print
+bk.sendDocument(documentId, mode, color).then((res) => {
+    console.log(res); // return orderId and price in €
+});
+
+
+// orderId: result of sendDocument
+bk.statusDocument(orderId).then((res) => {
+    console.log(res); // return status, tracking and created state
+});
---
+++ app.js
@@ -0,0 +1,111 @@
+const axios = require('axios');
+const FormData = require('form-data');
+
+const apiURL = "https://api.briefklick.de/v2/";
+
+const briefklick = class{
+    constructor(apikey){
+        this.apikey = apikey;
+    }
+
+    getBalance() {
+        var bodyFormData = new FormData();
+        bodyFormData.append('apikey', this.apikey);
+
+        return axios({
+            method: "get",
+            url: apiURL + "balance",
+            data: bodyFormData,
+            headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then(function (response) {
+            return response.data;
+        })
+        .catch(function (response) {
+            return response.data;
+        });
+    }
+
+    createDocument(sender, receiver, html, extraPage) {
+        var bodyFormData = new FormData();
+        bodyFormData.append('apikey', this.apikey);
+        bodyFormData.append('sender', sender);
+        bodyFormData.append('receiver', receiver);
+        bodyFormData.append('html', html);
+        bodyFormData.append('extraPage', extraPage);
+
+        return axios({
+            method: "post",
+            url: apiURL + "create",
+            data: bodyFormData,
+            headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then(function (response) {
+            return response.data;
+        })
+        .catch(function (response) {
+            return response.data;
+        });
+    }
+
+    previewDocument(documentId) {
+        var bodyFormData = new FormData();
+        bodyFormData.append('apikey', this.apikey);
+        bodyFormData.append('id', documentId);
+
+        return axios({
+            method: "post",
+            url: apiURL + "preview",
+            data: bodyFormData,
+            headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then(function (response) {
+            return response.data;
+        })
+        .catch(function (response) {
+            return response.data;
+        });
+    }
+
+    sendDocument(documentId, mode, color) {
+        var bodyFormData = new FormData();
+        bodyFormData.append('apikey', this.apikey);
+        bodyFormData.append('id', documentId);
+        bodyFormData.append('einschreiben', mode);
+        bodyFormData.append('farbig', color);
+
+        return axios({
+            method: "post",
+            url: apiURL + "send",
+            data: bodyFormData,
+            headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then(function (response) {
+            return response.data;
+        })
+        .catch(function (response) {
+            return response.data;
+        });
+    }
+
+    statusDocument(documentId) {
+        var bodyFormData = new FormData();
+        bodyFormData.append('apikey', this.apikey);
+        bodyFormData.append('id', documentId);
+
+        return axios({
+            method: "post",
+            url: apiURL + "status",
+            data: bodyFormData,
+            headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then(function (response) {
+            return response.data;
+        })
+        .catch(function (response) {
+            return response.data;
+        });
+    }
+}
+
+module.exports = briefklick;
